ddbebd6a454e37bb41ebb233224965343a26c657
zeekgwang <zeekgwang@gmail.com>
2021-05-30 17:22:58 +0900

    init

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/freedraw/<epi>', methods=['GET'])
+def freedraw(epi):
+    print(epi)
+
+    return render_template('frame.html',
+                           epi=epi,
+                           file_name=['20170830_163818.jpg',
+                                      '20170830_163818.jpg',
+                                      '20170830_163818.jpg',
+                                      '20170830_163818.jpg',
+                                      '20170830_163818.jpg',
+                                      '20170830_163818.jpg',
+                                      '20170830_163818.jpg'])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
